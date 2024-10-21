b3530b73061822fa9ce151a1efa5f2eb6621d7b9
anjulij <anjuli.jones73@gmail.com>
2024-10-21 11:58:23 -0400

    added subway sample data

---
+++ data/sql/create_table_subway_data.sql
@@ -0,0 +1,14 @@
+CREATE TABLE bus_data (
+    transit_timestamp TIMESTAMP,
+    transit_mode TEXT,
+    station_complex_id TEXT,  
+    station_complex TEXT,
+    borough TEXT,
+    payment_method TEXT,
+    fare_class_category TEXT,
+    ridership INTEGER,
+    transfers INTEGER.
+    latitude FLOAT,
+    longitude FLOAT,
+    Georeference FLOAT
+);
